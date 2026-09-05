fdaf9062bec3720c178f3bf7e84c143c46fd1d47
NIKHIL SONAR <nikhilsonar853@gmail.com>
2026-09-05 13:10:25 +0530

    Add HitBoxDamage script

--- Scripts/hitBoxDamage.cs
+++ Scripts/hitBoxDamage.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class HitBoxDamage : MonoBehaviour
 {
     private PlayerController controller;
+    private  Animator playerAnim;
     public int Damage = 50;
 
     private void Start()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         controller = player.GetComponent<PlayerController>();
+        playerAnim = player.GetComponent<Animator>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             controller.TeckDemage(Damage);
+            playerAnim.SetTrigger("sheck");
            
         }
     }
+}
+
