1562f6c9056855da59d6262289dce49c04e18ff3
NIKHIL SONAR <nikhilsonar853@gmail.com>
2026-09-05 13:07:46 +0530

    Update swipe handling in Swipe.cs
    Refactor swipe handling to use BaseEventData and update swipeDelta.

--- Scripts/Swipe.cs
+++ Scripts/Swipe.cs
@@ -1,21 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Swipe : MonoBehaviour
 {
     public float sensitivty = 0.5f;
-    public static Vector2 swipeDelta;
-    
+    public Vector2 swipeDelta;
+
     private void Update()
     {
-        
+
     }
     // Start is called before the first frame update
-    public void OnSwipe(PointerEventData eventData)
+    public void OnSwipe(BaseEventData eventData)
     {
-        swipeDelta = eventData.delta * sensitivty;
+        PointerEventData pointerData = eventData as PointerEventData;
+        if (pointerData != null)
+        {
+            swipeDelta = pointerData.delta * sensitivty;
+        }
+        
 
     }
 }
